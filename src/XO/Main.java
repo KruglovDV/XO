42145574ea689f1a8faf2110b79272be8e973176
Dmitry Kruglov <kruglov081289@mail.ru>
2017-12-09 19:12:08 +0300

    Feature: add console view and game loop

---
+++ src/XO/Main.java
@@ -0,0 +1,60 @@
+package XO;
+
+import XO.model.*;
+import XO.model.exceptions.*;
+import XO.controller.*;
+import XO.view.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(final String[] args) {
+        Scanner in = new Scanner(System.in);
+        final Game game = new Game(new Field());
+        final ConsoleView view = new ConsoleView(game);
+        final MoveController moveController = new MoveController();
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        final WinnerController winnerController = new WinnerController();
+        final Field field = game.getField();
+        final Main main = new Main();
+        main.gameLoop(view, currentMoveController, moveController, winnerController, in, field);
+    }
+
+    private void gameLoop(final ConsoleView view,
+                          final CurrentMoveController currentMoveController,
+                          final MoveController moveController,
+                          final WinnerController winnerController,
+                          final Scanner in,
+                          final Field field) {
+        int counter = 0;
+        do {
+            if (counter == 9) {
+                System.out.println("nobody win :(");
+                break;
+            }
+            final Figure figure = currentMoveController.getCurrentMove(field);
+            System.out.println("current move " + figure.toString());
+            view.printField();
+            System.out.println("input coordinate x");
+            int x = in.nextInt();
+            System.out.println("input coordinate y");
+            int y = in.nextInt();
+            try {
+                moveController.applyFigure(field, new Point(x, y), figure);
+                final Figure winnerFigure = winnerController.checkWinner(field);
+                if (winnerFigure != null) {
+                    view.printField();
+                    System.out.println(winnerFigure + " win!!!");
+                    break;
+                }
+            } catch (final AlreadyOccupiedException e) {
+                System.out.println("this coordinate is already occupied!");
+                continue;
+            } catch (final InvalidCoordinateException e) {
+                System.out.println("invalid coordinate!!!");
+                continue;
+            }
+            counter++;
+            view.printField();
+        } while (true);
+    }
+}
---
+++ src/XO/view/ConsoleView.java
@@ -0,0 +1,35 @@
+package XO.view;
+
+import XO.model.*;
+import XO.model.exceptions.InvalidCoordinateException;
+
+public class ConsoleView {
+    final private Game game;
+
+    public ConsoleView(final Game game) {
+        this.game = game;
+    }
+
+    public void printField() {
+        final Field field = this.game.getField();
+        for(int y = 0; y < field.getSize(); y++) {
+            try {
+                printLine(field.getFigure(new Point(0, y)),
+                          field.getFigure(new Point(1, y)),
+                          field.getFigure(new Point(2, y)));
+                if (y != 2) printSep();
+            } catch (final InvalidCoordinateException e) {}
+        }
+    }
+
+    private void printLine(final Figure point1, final Figure point2, final Figure point3) {
+        final String fig1 = point1 != null ? point1.toString() : " ";
+        final String fig2 = point2 != null ? point2.toString() : " ";
+        final String fig3 = point3 != null ? point3.toString() : " ";
+        System.out.println(" " + fig1 + " | " + fig2 + " | " + fig3);
+    }
+
+    private void printSep() {
+        System.out.println("-----------");
+    }
+}
